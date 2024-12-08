280c29a0a9a381e788c007a70dd34dd1acc38662
Latherver <mzfengibb@outlook.com>
2024-12-08 16:23:22 +0000

    Faster SVD and stable GaLore using SVD lowrank decomposition

--- galore_torch/galore_projector.py
+++ galore_torch/galore_projector.py
@@ -1,72 +1,28 @@
 import torch
 
 class GaLoreProjector:
-    def __init__(self, rank, verbose=False, update_proj_gap=200, scale=1.0, proj_type='std'):
+    def __init__(self, rank = 128, update_proj_gap=None):
         self.rank = rank
-        self.verbose = verbose
+        if update_proj_gap is None:
+            update_proj_gap = int(32 * rank / 256) # assert rank >= 8
         self.update_proj_gap = update_proj_gap
-        self.scale = scale
-        self.ortho_matrix = None
-        self.proj_type = proj_type
+        self.lowrank_svd = None
+        self.scale = None
 
-    def project(self, full_rank_grad, iter):
-        if self.proj_type == 'std':
-            if full_rank_grad.shape[0] >= full_rank_grad.shape[1]:
-                if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
-                    self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='right')
-                low_rank_grad = torch.matmul(full_rank_grad, self.ortho_matrix.t().to(full_rank_grad.device.type))
-            else:
-                if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
-                    self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='left')
-                low_rank_grad = torch.matmul(self.ortho_matrix.t().to(full_rank_grad.device.type), full_rank_grad)
-        elif self.proj_type == 'reverse_std':
-            if full_rank_grad.shape[0] >= full_rank_grad.shape[1]:
-                if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
-                    self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='left')
-                low_rank_grad = torch.matmul(self.ortho_matrix.t().to(full_rank_grad.device.type),full_rank_grad)
-            else:
-                if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
-                    self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='right')
-                low_rank_grad = torch.matmul(full_rank_grad,self.ortho_matrix.t().to(full_rank_grad.device.type))
-        elif self.proj_type == 'right':
-            if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
-                self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='right')
-            low_rank_grad = torch.matmul(full_rank_grad, self.ortho_matrix.t().to(full_rank_grad.device.type))
-        elif self.proj_type == 'left':
-            if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
-                self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='left')
-            low_rank_grad = torch.matmul(self.ortho_matrix.t().to(full_rank_grad.device.type), full_rank_grad)
-        elif self.proj_type == 'full':
-            if self.ortho_matrix is None or iter % self.update_proj_gap == 0:
-                self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='full')
-            low_rank_grad = torch.matmul(self.ortho_matrix[0].t().to(full_rank_grad.device.type), full_rank_grad) @ self.ortho_matrix[1].t().to(full_rank_grad.device.type)
+    def project(self, param, iter):
+        if self.lowrank_svd is None or iter % self.update_proj_gap == 0:
+            self.lowrank_svd = self.get_lowrank_svd(param, self.rank)
+        low_rank_grad = torch.matmul(self.lowrank_svd[0].t().to(param.grad.device.type), param.grad) @ self.lowrank_svd[2].to(param.grad.device.type) / self.lowrank_svd[1].to(param.grad.device.type)
 
         return low_rank_grad
 
     def project_back(self, low_rank_grad):
-        if self.proj_type == 'std':
-            if low_rank_grad.shape[0] >= low_rank_grad.shape[1]:
-                full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix.to(low_rank_grad.device.type))
-            else:
-                full_rank_grad = torch.matmul(self.ortho_matrix.to(low_rank_grad.device.type), low_rank_grad)
-        elif self.proj_type == 'reverse_std':
-            if low_rank_grad.shape[0] <= low_rank_grad.shape[1]: # note this is different from std
-                full_rank_grad = torch.matmul(self.ortho_matrix.to(low_rank_grad.device.type), low_rank_grad)
-            else:
-                full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix.to(low_rank_grad.device.type))
-        elif self.proj_type == 'right':
-            full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix.to(low_rank_grad.device.type))
-        elif self.proj_type == 'left':
-            full_rank_grad = torch.matmul(self.ortho_matrix.to(low_rank_grad.device.type), low_rank_grad)
-        elif self.proj_type == 'full':
-            full_rank_grad = torch.matmul(self.ortho_matrix[0].to(low_rank_grad.device.type), low_rank_grad) @ self.ortho_matrix[1].to(low_rank_grad.device.type)
-
-
-        return full_rank_grad * self.scale
+        restored_grad = torch.matmul(self.lowrank_svd[0].to(low_rank_grad.device.type), low_rank_grad * self.lowrank_svd[1].to(low_rank_grad.device.type)) @ self.lowrank_svd[2].t().to(low_rank_grad.device.type)
+        return restored_grad * self.scale
 
 
     # svd decomposition
-    def get_orthogonal_matrix(self, weights, rank, type):
+    def get_lowrank_svd(self, weights, rank):
         module_params = weights
 
         if module_params.data.dtype != torch.float:
@@ -78,25 +34,17 @@ class GaLoreProjector:
             float_data = True
             matrix = module_params.data
 
-        U, s, Vh = torch.linalg.svd(matrix, full_matrices = False)
+        U, s, V = torch.svd_lowrank(matrix, q=rank, niter=2)
+        if not float_data:
+            U = U.to(original_device).type(original_type)
+            s = s.to(original_device).type(original_type)
+            V = V.to(original_device).type(original_type)
 
-        #make the smaller matrix always to be orthogonal matrix
-        if type=='right':
-            B = Vh[:rank, :]
-            if not float_data:
-                B = B.to(original_device).type(original_type)
-            return B
-        elif type=='left':
-            A = U[:, :rank]
-            if not float_data:
-                A = A.to(original_device).type(original_type)
-            return A
-        elif type=='full':
-            A = U[:, :rank]
-            B = Vh[:rank, :]
-            if not float_data:
-                A = A.to(original_device).type(original_type)
-                B = B.to(original_device).type(original_type)
-            return [A, B]
-        else:
-            raise ValueError('type should be left, right or full')
+        if not self.scale:
+            dim = min(module_params.data.shape[0], module_params.data.shape[1])
+            from math import log2
+            self.scale = dim * log2(dim) / (rank * log2(rank))
+            # from math import sqrt
+            # self.scale = dim * sqrt(dim) / (rank * sqrt(rank))
+
+        return U, s, V
